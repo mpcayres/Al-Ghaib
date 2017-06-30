619d32f0da88694543a8137eaeefdb2d704bc3f7
Rennê Ruan <140030930@aluno.unb.br>
2017-06-30 19:37:11 -0300

    "Path Finding"

--- include/TileMap.hpp
+++ include/TileMap.hpp
@@ -3,6 +3,11 @@
 
 #include <string>
 #include <vector>
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
+#include <set>
+#include <queue>
 
 #include "TileSet.hpp"
 #include "Geometry.hpp"
@@ -19,17 +24,39 @@ public:
 	int GetWidth();
 	int GetHeight();
 	int GetDepth();
+	std::queue<Vec2> GetPath();
 	Rect FindLimits();
 	std::vector<Rect> GetInnerLimits();
+	void PathFind(Vec2 origem, Vec2 destino);
 
 private:
+	typedef std::pair<int, int> Pair;
+	typedef std::pair<double, std::pair<int, int> > pPair;
+
+	struct cell{
+	    int parent_i, parent_j;
+	    double f, g, h;
+	};
+
+	bool isValid(int row, int col);
+	bool isUnBlocked(std::vector<std::vector<int> > grid, int row, int col);
+	bool isDestination(int row, int col, Pair dest);
+	double calculateHValue(int row, int col, Pair dest);
+	void tracePath(std::vector<std::vector<cell>> cellDetails, Pair dest);
+	void aStarSearch(std::vector<std::vector<int>> grid, Pair src, Pair dest);
+
+
 	std::vector<int> tileMatrix;
+	std::vector<std::vector<int>> grid;
 	std::vector<Rect> limitsVector;
 	TileSet* tileSet;
 	int mapWidth;
 	int mapHeight;
 	int mapDepth;
 	int mapInsideSquares;
+
+	bool hasPath;
+	std::queue<Vec2> path;
 };
 
 #endif /* INCLUDE_TILEMAP_HPP_ */
--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -5,6 +5,7 @@
 
 TileMap::TileMap(std::string file, TileSet* tileSet){
 	this->tileSet = tileSet;
+	hasPath = false;
 	Load(file);
 }
 
@@ -26,9 +27,24 @@ void TileMap::Load(std::string file){
 		limitsVector.push_back(Rect((x * wiAux), (y*heAux), (c*wiAux), (a*heAux)));
 	}
 
+	for (int i = 0; i < mapHeight; i++) {
+	    std::vector<int> row;
+	    for (int j = 0; j < mapWidth; j++) {
+	        row.push_back(0);
+	    }
+	    grid.push_back(row);
+	}
+
+	int m=0,n=0;
 	while(!feof(fp)){
 		fscanf(fp, " %d,", &num);
 		tileMatrix.push_back(num - 1);
+		if(num == 1) grid[m][n] = 1;
+		n++;
+		if(n == mapWidth){
+			n = 0;
+			m++;
+		}
 	}
 
 	fclose(fp);
@@ -106,4 +122,267 @@ Rect TileMap::FindLimits(){
 
 std::vector<Rect> TileMap::GetInnerLimits(){
 	return limitsVector;
+}
+
+bool TileMap::isValid(int row, int col){
+    return (row >= 0) && (row < mapHeight) &&
+           (col >= 0) && (col < mapWidth);
+}
+
+bool TileMap::isUnBlocked(std::vector<std::vector<int>> grid, int row, int col){
+    if (grid[row][col] == 1)
+        return (true);
+    else
+        return (false);
+}
+
+bool TileMap::isDestination(int row, int col, Pair dest){
+    if (row == dest.first && col == dest.second)
+        return (true);
+    else
+        return (false);
+}
+
+double TileMap::calculateHValue(int row, int col, Pair dest){
+    return ((double)sqrt ((row-dest.first)*(row-dest.first) + (col-dest.second)*(col-dest.second)));
+}
+
+void TileMap::tracePath(std::vector<std::vector<cell>> cellDetails, Pair dest){
+   // printf ("\nThe Path is ");
+    int row = dest.first;
+    int col = dest.second;
+
+    std::stack<Pair> Path;
+
+    while (!(cellDetails[row][col].parent_i == row
+             && cellDetails[row][col].parent_j == col )){
+        Path.push(std::make_pair (row, col));
+        int temp_row = cellDetails[row][col].parent_i;
+        int temp_col = cellDetails[row][col].parent_j;
+        row = temp_row;
+        col = temp_col;
+    }
+
+    Path.push(std::make_pair (row, col));
+
+    while (!Path.empty()){
+        std::pair<int,int> p = Path.top();
+        path.emplace(Vec2((p.second)*tileSet->GetTileWidth() + tileSet->GetTileWidth()/2,
+        		(p.first)*tileSet->GetTileHeight() + tileSet->GetTileHeight()/2 ));
+
+        Path.pop();
+       // printf("-> (%d,%d) ",p.first,p.second);
+    }
+    hasPath = true;
+}
+
+void TileMap::aStarSearch(std::vector<std::vector<int>> grid, Pair src, Pair dest){
+
+    if (isValid (src.first, src.second) == false){
+        printf ("Source is invalid\n");
+        return;
+    }
+
+    if (isValid (dest.first, dest.second) == false){
+        printf ("Destination is invalid\n");
+        return;
+    }
+
+    if (isUnBlocked(grid, src.first, src.second) == false ||
+            isUnBlocked(grid, dest.first, dest.second) == false){
+        printf ("Source or the destination is blocked\n");
+        return;
+    }
+
+    if (isDestination(src.first, src.second, dest) == true){
+        printf ("We are already at the destination\n");
+        return;
+    }
+
+    bool closedList[mapHeight][mapWidth];
+    memset(closedList, false, sizeof (closedList));
+
+    std::vector<std::vector<cell>> cellDetails(mapHeight, std::vector<cell>(mapWidth));
+    int i, j;
+    for (i=0; i<mapHeight; i++){
+        for (j=0; j<mapWidth; j++){
+            cellDetails[i][j].f = FLT_MAX;
+            cellDetails[i][j].g = FLT_MAX;
+            cellDetails[i][j].h = FLT_MAX;
+            cellDetails[i][j].parent_i = -1;
+            cellDetails[i][j].parent_j = -1;
+        }
+    }
+
+    i = src.first, j = src.second;
+    cellDetails[i][j].f = 0.0;
+    cellDetails[i][j].g = 0.0;
+    cellDetails[i][j].h = 0.0;
+    cellDetails[i][j].parent_i = i;
+    cellDetails[i][j].parent_j = j;
+
+    std::set<pPair> openList;
+
+    openList.insert(std::make_pair (0.0, std::make_pair (i, j)));
+
+    bool foundDest = false;
+
+    while (!openList.empty()){
+        pPair p = *openList.begin();
+
+        openList.erase(openList.begin());
+        i = p.second.first;
+        j = p.second.second;
+        closedList[i][j] = true;
+
+        double gNew, hNew, fNew;
+        if (isValid(i-1, j) == true){
+
+            if (isDestination(i-1, j, dest) == true){
+                cellDetails[i-1][j].parent_i = i;
+                cellDetails[i-1][j].parent_j = j;
+            //    printf ("The destination cell is found\n");
+                tracePath (cellDetails, dest);
+                foundDest = true;
+                return;
+            }
+            else if (closedList[i-1][j] == false &&
+                     isUnBlocked(grid, i-1, j) == true){
+                gNew = cellDetails[i][j].g + 1.0;
+                hNew = calculateHValue (i-1, j, dest);
+                fNew = gNew + hNew;
+
+                if (cellDetails[i-1][j].f == FLT_MAX ||
+                        cellDetails[i-1][j].f > fNew){
+                    openList.insert( std::make_pair(fNew,
+                                              std::make_pair(i-1, j)));
+
+
+                    cellDetails[i-1][j].f = fNew;
+                    cellDetails[i-1][j].g = gNew;
+                    cellDetails[i-1][j].h = hNew;
+                    cellDetails[i-1][j].parent_i = i;
+                    cellDetails[i-1][j].parent_j = j;
+                }
+            }
+        }
+        if (isValid(i+1, j) == true){
+
+            if (isDestination(i+1, j, dest) == true){
+
+                cellDetails[i+1][j].parent_i = i;
+                cellDetails[i+1][j].parent_j = j;
+             //   printf("The destination cell is found\n");
+                tracePath(cellDetails, dest);
+                foundDest = true;
+                return;
+            }
+
+            else if (closedList[i+1][j] == false &&
+                     isUnBlocked(grid, i+1, j) == true){
+                gNew = cellDetails[i][j].g + 1.0;
+                hNew = calculateHValue(i+1, j, dest);
+                fNew = gNew + hNew;
+
+                if (cellDetails[i+1][j].f == FLT_MAX ||
+                        cellDetails[i+1][j].f > fNew){
+                    openList.insert( std::make_pair (fNew, std::make_pair (i+1, j)));
+                    cellDetails[i+1][j].f = fNew;
+                    cellDetails[i+1][j].g = gNew;
+                    cellDetails[i+1][j].h = hNew;
+                    cellDetails[i+1][j].parent_i = i;
+                    cellDetails[i+1][j].parent_j = j;
+                }
+            }
+        }
+
+        if (isValid (i, j+1) == true){
+
+            if (isDestination(i, j+1, dest) == true){
+                cellDetails[i][j+1].parent_i = i;
+                cellDetails[i][j+1].parent_j = j;
+            //    printf("The destination cell is found\n");
+                tracePath(cellDetails, dest);
+                foundDest = true;
+                return;
+            }
+
+
+            else if (closedList[i][j+1] == false &&
+                     isUnBlocked (grid, i, j+1) == true){
+                gNew = cellDetails[i][j].g + 1.0;
+                hNew = calculateHValue (i, j+1, dest);
+                fNew = gNew + hNew;
+
+
+                if (cellDetails[i][j+1].f == FLT_MAX ||
+                        cellDetails[i][j+1].f > fNew){
+                    openList.insert( std::make_pair(fNew,
+                                        std::make_pair (i, j+1)));
+
+                    cellDetails[i][j+1].f = fNew;
+                    cellDetails[i][j+1].g = gNew;
+                    cellDetails[i][j+1].h = hNew;
+                    cellDetails[i][j+1].parent_i = i;
+                    cellDetails[i][j+1].parent_j = j;
+                }
+            }
+        }
+
+
+
+
+        if (isValid(i, j-1) == true){
+            if (isDestination(i, j-1, dest) == true){
+
+                cellDetails[i][j-1].parent_i = i;
+                cellDetails[i][j-1].parent_j = j;
+           //     printf("The destination cell is found\n");
+                tracePath(cellDetails, dest);
+                foundDest = true;
+                return;
+            }
+
+
+            else if (closedList[i][j-1] == false &&
+                     isUnBlocked(grid, i, j-1) == true){
+                gNew = cellDetails[i][j].g + 1.0;
+                hNew = calculateHValue(i, j-1, dest);
+                fNew = gNew + hNew;
+
+
+                if (cellDetails[i][j-1].f == FLT_MAX ||
+                        cellDetails[i][j-1].f > fNew){
+                    openList.insert( std::make_pair (fNew,
+                                         std::make_pair (i, j-1)));
+
+                    cellDetails[i][j-1].f = fNew;
+                    cellDetails[i][j-1].g = gNew;
+                    cellDetails[i][j-1].h = hNew;
+                    cellDetails[i][j-1].parent_i = i;
+                    cellDetails[i][j-1].parent_j = j;
+                }
+            }
+        }
+    }
+    if (foundDest == false)
+        printf("Failed to find the Destination Cell\n");
+
+    return;
+}
+
+
+void TileMap::PathFind(Vec2 origem, Vec2 destino){
+	    Pair src = std::make_pair(origem.y/tileSet->GetTileHeight(), origem.x/tileSet->GetTileWidth());
+	    Pair dest = std::make_pair(destino.y/tileSet->GetTileHeight(), destino.x/tileSet->GetTileWidth());
+	    aStarSearch(grid, src, dest);
+}
+
+std::queue<Vec2> TileMap::GetPath(){
+	std::queue<Vec2> aux;
+	if(hasPath){
+		hasPath = false;
+		return path;
+	}
+	else return(aux);
 }
